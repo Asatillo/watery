f2d6a33f52df8051b50a91eedd5d3a51d4d696ac
asatik <asatillo.akhmedov@gmail.com>
2024-10-09 20:16:56 +0200

    set the notification

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
-chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === 'waterReminder') {
-        chrome.notifications.create({
-            type: 'basic',
-            iconUrl: 'images/icon48.png',
-            title: 'Time to Drink Water',
-            message: 'Stay hydrated! Drink some water now.',
-            priority: 2
-        });
-    }
+'use strict';
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: '/assets/logos/watery128.png',
+    title: chrome.runtime.getManifest().name,
+    message: "It's time to drink some water!",
+    buttons: [{ title: 'Keep it Flowing.' }],
+    priority: 0
+  });
 });
